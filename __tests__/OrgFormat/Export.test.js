5cf26716771c4b4b5f2dd12bae0e021fe31f6e06
Bartłomiej Nankiewicz <bartlomiej.nankiewicz@gmail.com>
2018-03-30 17:25:23 +0200

    Fix parsing multiple drawers

--- __tests__/OrgFormat/Export.test.js
+++ __tests__/OrgFormat/Export.test.js
@@ -4,9 +4,15 @@ import R from "ramda";
 
 import { createNewNode } from '../../src/OrgFormat/Export';
 import { parse } from '../../src/OrgFormat/Parser';
+import { getOrgFileContent } from "../../src/Helpers/Fixtures";
 
 test("createNewNode", () => {
   const rawNode = ["* This is headline :tag1:tag2:",
                    "content"];
-  expect(createNewNode(parse(rawNode)[0])).toEqual(rawNode.join('\n'));
-});
+  expect(createNewNode(parse(rawNode)[0])).toEqual(rawNode.join('\n'));});
+
+
+test.only("full parse-export test", () => {
+  const parsedNodes = parse(getOrgFileContent("parse-export-test.org"))
+  console.log(parsedNodes)
+})
--- src/OrgFormat/GenericParsers/LinesRangeParser.js
+++ src/OrgFormat/GenericParsers/LinesRangeParser.js
@@ -5,33 +5,36 @@ import R from "ramda";
 import { parser } from './Common';
 import { selfChangingRegexMatch } from '../../Helpers/Functions';
 
-// Wycina z objektu z liniami bloki oddzielone dwoma wyrażeniami regularnymi.
+// Slices area delimited by two regex expressions
+
+const isOdd = R.pipe(R.length, R.modulo(R.__, 2))
+const dropRetardedDrawersAndGroupByTwo = R.pipe(
+  // R.when(isOdd, R.dropLast(1)),
+  R.splitEvery(2))
 
 const linesRangeParser = (regex, transform, wrapIn=null) => {
-  const terminator = (acc) => acc.length == 2 ? R.reduced(acc) : acc;
-
-  // Transform will extract drawer name from first tag
   const parseLine = R.pipe(
     selfChangingRegexMatch(regex),
     R.unless(R.isEmpty, R.apply(transform)))
 
   const makeOutput = (inputObjects, inputLines, parserOutput) => {
-    const isOdd = R.modulo(R.__, 2)
-    const prepare = R.pipe(
-      R.when(isOdd, R.dropLast(1)),
-      R.splitEvery(2))
+    let drawers = []
+    let nrOfAlreadySlicedLines = 0
+    R.forEach((parsed) => {
+      console.log(parsed)
+      const lineStart = parsed[0].lineNr - nrOfAlreadySlicedLines
+      const lineEnd = parsed[1].lineNr - nrOfAlreadySlicedLines +1
+      nrOfAlreadySlicedLines += lineEnd - lineStart
 
-    R.forEach(
-      (parsed) => {
-        const objStart = parsed[0]
-        const objEnd = parsed[1]
-        const objName = objStart.parsedObj
-        const drawerContent = inputLines.splice(objStart.lineNr, objEnd.lineNr+1)
-        const obj = { drawers: { [objName]: drawerContent.slice(1,-1) } }
-        inputObjects.push(obj)}
-      , prepare(parserOutput))
+      const objName = parsed[0].parsedObj
+      const drawerContent = inputLines.splice(lineStart, lineEnd - lineStart)
+      drawers.push({ [objName]: drawerContent.slice(1,-1) })
+
+    } , dropRetardedDrawersAndGroupByTwo(parserOutput))
+
+    console.log(drawers)
+    if (drawers.length > 0) inputObjects.push({ drawers })
+
     return [inputObjects, inputLines]}
-
-  return parser(parseLine, makeOutput, terminator)}
-
+  return parser(parseLine, makeOutput)}
 export { linesRangeParser }
