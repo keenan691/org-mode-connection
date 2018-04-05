ff5e9e00bb7b22f5671da5a07d64a37897ea4813
Bartłomiej Nankiewicz <bartlomiej.nankiewicz@gmail.com>
2018-04-06 01:33:53 +0200

    Add support for react-native-fs

--- src/Helpers/FileAccess.js
+++ src/Helpers/FileAccess.js
@@ -1,42 +1,38 @@
 // @flow
 
 import R from "ramda";
-// import fs from "fs";
-import path from "path";
-var promisify = require("promisify-node");
-var fs = promisify("fs");
+
+let FileSystems = {}
+let FileSystemsInterfaces = {}
+
 // * NodeJS file handling
 
-
+try {
+  var promisify = require("promisify-node");
+  FileSystems['fs'] = promisify("fs")
+  console.log('node fs registered')
+} catch (error) {
+  // console.log(error)
+}
 
 // * React Native file handling
-// RNFS.readFile(filepath).then(contents=>)
-// const RNFS = require('react-native-fs')
-// export function getFilesWithExternalChanges(orgFilesState=temp) {
-//   return Promise.all(orgFilesState.map(file => RNFS.exists(file.path)))
-//     .then(filesExists => {
-//       const existingFiles = orgFilesState.filter((v, i) => filesExists[i]);
-//       const deletedFiles = orgFilesState.filter((v, i) => !filesExists[i]);
-//       return { deletedFiles, existingFiles };
-//     })
-//     .then(({ existingFiles, deletedFiles }) => {
-//       return Promise.all(existingFiles.map(file => RNFS.stat(file.path)))
-//         .then(stats => {
-//           const changedFiles = existingFiles.filter((v, i) => v.lastSync <
-//                                                     stats[i].mtime);
-//           // Flat results
-//           return {
-//             changed: changedFiles.map(o => o.path),
-//             deleted: deletedFiles.map(o => o.path)
-//           };
-//         });
-//     });
-// }
 
-// * Facade
+try {
+  FileSystems['RNFS'] = require("react-native-fs")
+  console.log('react native fs registered')
+} catch (error) {
+  // console.log(error)
+}
+
+// * Select default filesystem
+
+const defaultFS = FileSystems.RNFS ? FileSystems.RNFS : FileSystems.fs
+
+// * Api
 
 export default {
-  stat: (path) => fs.stat(path),
+  stat: (path) => defaultFS.stat(path),
   write (path, content) {},
-  read: (path) => fs.readFile(path, 'utf8').then(content => content.split('\n'))
-};
+  read: (path) => defaultFS.readFile(path, 'utf8').then(
+    content => content.split('\n'))
+}
