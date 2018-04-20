a74ffd3f9c1d76b4a4514bc08dcf2228779c4766
Bartłomiej Nankiewicz <bartlomiej.nankiewicz@gmail.com>
2018-04-20 14:00:54 +0200

    Add tests data for content to objects mapping

--- __tests__/Data/Transforms.test.js
+++ __tests__/Data/Transforms.test.js
@@ -1,10 +1,11 @@
-import R from "ramda";
+// * TransformTest starts here
 
+import R from "ramda";
 import { mapNodeContentToObject } from '../../src/Data/Transforms';
 
-// * Input data
+// * Factory functions
 
-// ** Data creators
+// ** Node
 
 const createNode = (props) => ({
   headline: 'This is headline ',
@@ -12,85 +13,195 @@ const createNode = (props) => ({
   // ...props
 })
 
-// ** Data elements
+// ** Inline elements
 
-const elements = {
-  links: {
-    webLine: ' [[https://reactnavigation.org/docs/drawer-navigator.html][DrawerNavigator reference · React Navigation]]',
-    mailLine: ' [[https://reactnavigation.org/docs/drawer-navigator.html][DrawerNavigator reference · React Navigation]]',
-    unknownLine: ' [[https://reactnavigation.org/docs/drawer-navigator.html][DrawerNavigator reference · React Navigation]]'},
-  lines: {
-    emptyLine: '\n',
-    listLine: '- Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-    checkboxLine: '- [ ] Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-    checkboxLineWithBold: '- [ ] *Lorem* ipsum dolor sit amet, consectetuer adipiscing elit.',
-    numericListLine: '1. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-    regularLine: 'Suspendisse potenti.  ',
-    strikeThroughLine: 'Proin quam nisl, +tincidunt+ et, mattis eget, convallis nec, purus.  ',
-    underlineLine: 'Nunc aliquet, augue _nec_ adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio.  ',
-    verbatimLine: '=Integer= placerat tristique nisl.  ',
-    boldLine: 'Fusce sagittis, libero non molestie mollis, *magna* orci ultrices dolor, at *vulputate* neque nulla *lacinia* eros.  ',
-    italicLine: 'Sed /bibendum/.  ',
-    codeLine: 'Vestibulum ~convallis~, lorem a tempus semper, dui dui euismod elit, vitae placerat urna tortor vitae lacus.  ',},}
+const link = (url, urlTitle) => ({
+  type: 'link',
+  text: `[[${url}][${urlTitle}]]`});
 
-// * Output data
-
-// ** Elements creators
-
-// *** Inline elements
-
-const regularTextCreator = (t) => {
+const regularText = (t) => ({
   type: 'regular',
-  text: t};
+  text: `${t}`});
 
-const strikeThroughTextCreator = (t) => {
+const codeText = (t) => ({
+  type: 'code',
+  text: `~${t}~`});
+
+const strikeThroughText = (t) => ({
   type: 'strikeThrough',
-  text: `+${t}+`};
+  text: `+${t}+`});
 
-const underlineTextCreator = (t) => {
+const underlineTextCreator = (t) => ({
   type: 'underline',
-  text: `_${t}_`};
+  text: `_${t}_`});
 
-const verbatimTextCreator = (t) => {
+const verbatimTextCreator = (t) => ({
   type: 'verbatim',
-  text: `=${t}=`};
+  text: `=${t}=`});
 
-const boldTextCreator = (t) => {
+const boldTextCreator = (t) => ({
   type: 'bold',
-  text: `*${t}*`};
+  text: `*${t}*`});
 
-const italicTextCreator = (t) => {
+const italicTextCreator = (t) => ({
   type: 'italic',
-  text: `/${t}/`};
+  text: `/${t}/`});
 
-// *** Line containers
+// ** Line containers
 
-const regularLine = (elements) => {
-  type: 'regular'
-  elements};
+const regularLine = (elements) => ({
+  type: 'regular',
+  elements});
 
-const listLine = (elements) => {
-  type: 'list'
-  elements};
+const listLine = (elements) => ({
+  type: 'list',
+  elements});
 
-const checkboxLine = (elements) => {
-  type: 'checkbox'
-  elements};
+const checkboxLine = (elements) => ({
+  type: 'checkbox',
+  elements});
 
-const numericListLine = (elements) => {
-  type: 'numericList'
-  elements};
+const numericListLine = (elements) => ({
+  type: 'numericList',
+  elements});
+
+// * Test data
+
+// ** Links
+
+const links = {
+
+  unknown: {
+    url: 'https://reactnavigation.org/docs/drawer-navigator.html ',
+    urlTitle: 'DrawerNavigator reference · React Navigation]]'}};
+
+const linksMappings = {
+  generic: ' [[https://reactnavigation.org/docs/drawer-navigator.html][DrawerNavigator reference · React Navigation]]',
+  web: ' [[https://reactnavigation.org/docs/drawer-navigator.html][DrawerNavigator reference · React Navigation]]',
+  mail: ' [[https://reactnavigation.org/docs/drawer-navigator.html][DrawerNavigator reference · React Navigation]]'};
+
+// ** Headers
+
+const headerMappings = {
+
+  regular: [
+    'Nulla posuere.',
+    [regularLine(
+      [regularText('Nulla posuere.')])]
+  ],
+
+  withLink: [
+    `Nulla posuere. ${link(...links.unknown)} Proin neque massa`,
+    [regularLine([
+      regularText('Nulla posuere.'),
+      link('')])]]};
+
+// ** Content
+
+const contentToObjectsMappings = {
+
+// *** Line types
+
+  emptyLine: [
+    '\n',
+    [regularLine(
+      [regularText('\n')])]],
+
+  regularLine: [
+    'Suspendisse potenti.  ',
+    [regularLine(
+      [regularText('Suspendisse potenti.  ')])]],
+
+  twoLines: [
+    'one\ntwo',
+    [regularLine(regularText('one')),
+     regularLine(regularText('two'))]
+  ],
+
+  listLine: [
+    '- Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    [listLine(
+      [regularText('Lorem ipsum dolor sit amet, consectetuer adipiscing elit.')])]],
+
+  checkboxLine: [
+    '- [ ] Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    [checkboxLine(
+      [regularText('Lorem ipsum dolor sit amet, consectetuer adipiscing elit.')])]],
+
+  checkboxLineWithBold: [
+    '- [ ] *Lorem* ipsum dolor sit amet, consectetuer adipiscing elit.',
+    [regularLine(
+      [boldTextCreator('Lorem'),
+       regularText('ipsum dolor sit amet, consectetuer adipiscing elit.')])]],
+
+  numericListLine: [
+    '1. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    [numericListLine(
+      [regularText('Lorem ipsum dolor sit amet, consectetuer adipiscing elit.')])]],
+
+// *** Faces
+
+  strikeThroughLine: [
+    'Proin quam nisl, +tincidunt+ et, mattis eget, +convallis+ nec, purus.  ',
+    [regularLine(
+      [regularText('Proin quam nisl, '),
+       strikeThroughText('tincidunt'),
+       regularText(' et, mattis eget, '),
+       strikeThroughText('convallis'),
+       regularText(' nec, purus.  ')])]],
+
+  underlineLine: [
+    'Nunc aliquet, augue _nec_ adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio.  ',
+    [regularLine(
+      [regularText('Nunc aliquet, augue') ,
+       underlineTextCreator('nec') ,
+       regularText(' adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio.  ')])]],
+
+  boldLine: [
+    'Fusce sagittis, libero non molestie mollis, *magna* orci ultrices dolor, at *vulputate* neque nulla *lacinia* eros.  ',
+    [regularLine(
+      [regularText('Fusce sagittis, libero non molestie mollis,') ,
+       boldTextCreator('magna') ,
+       regularText('orci ultrices dolor, at') ,
+       boldTextCreator('vulputate') ,
+       regularText('neque nulla') ,
+       boldTextCreator('lacinia') ,
+       regularText('eros.')])]],
+
+  codeLine: [
+    '~convallis~',
+    [regularLine(
+      [codeText('convallis')])]],
+
+  verbatimLine: [
+    '=Integer=',
+    [regularLine(
+      [verbatimTextCreator('Integer')])]],
+
+  italicLine: [
+    '/bibendum/',
+    [regularLine(
+      [italicTextCreator('bibendum')])]]
+
+  // ** end
+
+}
+
 
 // * Tests
 
 describe("mapNodeContentToObject", () => {
-  test.only("parsing regular simple node", () => {
+
+  test.only("returns input object in plainContent prop", () => {
     const testData = createNode();
-    const expectation = {
-      [[]]
-    };
-    expect(mapNodeContentToObject(testData).plainContent).toBe(testData)
-    expect(mapNodeContentToObject(testData).objectContent).toEqual(testData)
+    const expectation = testData;
+    expect(mapNodeContentToObject(testData).plainContent).toBe(expectation)})
+
+  test.only("mapping lines", () => {
+    const testData = ;
+    const expectation = ;
+    expect(mapNodeContentToObject(testData)).toBe(expectation)
   });
+
 
 })
